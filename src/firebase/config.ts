980b3c14c0c0acbc81520fd4781dec64280b6b32
Denny Imanuel <denny.imanuel.111@gmail.com>
2025-11-02 08:02:05 +0000

    there are missing firebase config file while trying to run 'npm run db:p

---
+++ src/firebase/config.ts
@@ -0,0 +1,14 @@
+
+// This file contains the configuration for your Firebase project.
+// You can get this information from the Firebase console.
+// Go to Project settings > General tab, scroll down to "Your apps", and select the web app.
+
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
